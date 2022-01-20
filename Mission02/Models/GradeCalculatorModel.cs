95409accca35781253c9003f6227db37c956f811
ShengLei123 <starkray666666@gmail.com>
2022-01-19 22:51:19 -0700

    Mission 3

---
+++ Mission02/Models/GradeCalculatorModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission02.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Range(0, 100)]
+        public int Assignments { get; set; }
+        public int Groupproject { get; set; }
+        public int Quizzes { get; set; }
+        public int Exams { get; set; }
+        public int Intex { get; set; }
+    }
+}
